f6f2779bff1973ebca09b4881788ed7167339200
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-08-22 15:21:32 -0300

    Adição barra lateral, mediaquery, funcionalidades JS

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
-window.addEventListener('scroll', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById('main-header');
+  const headerHeight = header.offsetHeight;
+  const navSideBar = document.querySelector('.nav-side-bar');
+  const overlaySideBar = document.querySelector('.overlay');
+  
+  // Lógica de rolagem para o header
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > headerHeight) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
 
-  if (window.scrollY > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
+  // Toggle para a barra lateral (nav-side-bar)
+  document.querySelector('.menu-button').addEventListener('click', function () {
+    navSideBar.classList.toggle('show');
+    overlaySideBar.classList.toggle('active');
+  });
+
+  // Fechar a barra lateral (nav-side-bar)
+  document.querySelector('.img-close-nav').addEventListener('click', function () {
+    navSideBar.classList.remove('show');
+    overlaySideBar.classList.toggle('active');
+  });
 });
